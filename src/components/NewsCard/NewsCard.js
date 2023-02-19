2b6c569d505a5ca8729663023574d450bcd244d5
Your Name <niktjawalla922@gmail.com>
2023-02-19 22:20:00 +0100

    add styling and link content for News Cards also add infoCards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from "@mui/material";
+import useStyles from "./styles.js"
 
-const NewsCard = () => {
+const NewsCard = ({article: {description, publishedAt, source, title, url, urlToImage}, i}) => {
+
+    const classes = useStyles()
+
     return (
-        <Card>
-            <CardActionArea>
-                <CardMedia/>
-                <div>
-                    <Typography variant="body2" color="textSecondary" component="h2"></Typography>
-                    <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target ="_blank">
+                <CardMedia className={classes.media} image={urlToImage || "https://img.freepik.com/vektoren-kostenlos/globaler-technologiehintergrund-mit-textplatz_1017-19388.jpg?w=996&t=st=1676840440~exp=1676841040~hmac=0968bc7c634f2e7b4df3d6e981c58a7f72a6cee57db7a2ffd5bbe136bec14270"}/>
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
                 </div>
-                <Typography gutterBottom variant="h5"></Typography> 
+                <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography> 
                 <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
                 </CardContent> 
             </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary"></Button>
-                <Typography variant="h5" color="textSecondary"></Typography>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
             </CardActions>
         </Card>
     )
