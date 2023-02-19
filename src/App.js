d848704c31aaf4f9c6c9e2b9befaab54975d10c2
Your Name <niktjawalla922@gmail.com>
2023-02-19 16:24:45 +0100

    import alanbtn. add newHeadline Command

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";      
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = "2274e5d6934e6a48bee60b6e94f584fd2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+useEffect(()=>{
+    alanBtn({
+        key: alanKey,
+        onCommand: ({command, articles}) => {
+            if(command === "newHeadlines"){
+                console.log(articles);
+            }
+        }
+    })
+},[])
     return (<div>
         <h1>Alan AI News Application</h1>
     </div>)
