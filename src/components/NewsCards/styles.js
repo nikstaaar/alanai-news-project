910a823d7e81f717d1f2c5271162f3d029ee69b3
Your Name <niktjawalla922@gmail.com>
2023-02-20 21:55:23 +0100

    padding fix

--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -31,7 +31,7 @@ export default makeStyles({
         width: "100%",
         height: "100%",
         padding: "5%",
-        borderRadius: "1.5%",
+        borderRadius: "1%",
         backgroundColor: "white",
         color: "black",
         boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
